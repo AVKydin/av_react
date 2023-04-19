a226a759e595b3880349f4737a2b1150fd75ab7b
Q1 <tlalok@ukr.net>
2023-04-20 00:05:24 +0300

    L3TS

---
+++ src/services/axios.service.ts
@@ -0,0 +1,15 @@
+import axios, {Axios, AxiosResponse} from "axios";
+
+import {baseURL} from "../configs/urls";
+
+type IRes<T> = Promise<AxiosResponse<T>>
+
+const axiosService = axios.create({baseURL});
+
+export {
+    axiosService
+}
+
+export type {
+    IRes
+}
