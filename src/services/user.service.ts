8ffa8a2c5a6983872c383761ea7764a8eb543b95
Q1 <tlalok@ukr.net>
2023-04-23 00:07:41 +0300

    start L4TS

---
+++ src/services/user.service.ts
@@ -0,0 +1,13 @@
+import {axiosService} from "./axios.service";
+import {urls} from "../constants/urls";
+import {IRes} from "../types/axiosRes.type";
+import {IUser} from "../Interfaces/user.interface";
+
+const userService={
+    getAll:():IRes<IUser[]>=>axiosService.get(urls.users.users),
+    getById:(id:number):IRes<IUser>=>axiosService.get(urls.users.byId(id))
+}
+
+export {
+    userService
+}
