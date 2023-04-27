9eb1dd4abb579ce5c3253544e47d619a78127a83
Q1 <tlalok@ukr.net>
2023-04-27 22:22:05 +0300

    start L6

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
+const baseReducer = (state, action)=>{
+    console.log(state, action)
+    return {...state}
+};
+
+const store = createStore(baseReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 
 );
